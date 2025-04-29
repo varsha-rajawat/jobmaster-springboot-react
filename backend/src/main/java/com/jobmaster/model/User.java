2bce740344f2363bb683323205dfc574116803eb
Varsha Rajawat <89792897+varsha-rajawat@users.noreply.github.com>
2025-04-29 16:15:59 -0400

    JobApplication CRUD completed for Backend (#3)
    * entity and repository setup
    
    * JobApplication CRUD completed for backend
    
    * remove JsonIgnore from user

--- backend/src/main/java/com/jobmaster/model/User.java
+++ backend/src/main/java/com/jobmaster/model/User.java
@@ -3,6 +3,8 @@ package com.jobmaster.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
