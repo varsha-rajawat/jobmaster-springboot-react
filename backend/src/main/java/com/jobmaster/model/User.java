54183798ec66f4b30e04e6e7c5e5df1dbac80942
learnerVarsha <learner.varsha@gmail.com>
2025-04-29 15:48:03 -0400

    JobApplication CRUD completed for backend

--- backend/src/main/java/com/jobmaster/model/User.java
+++ backend/src/main/java/com/jobmaster/model/User.java
@@ -40,5 +40,6 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<JobApplication> jobApplications = new ArrayList<>();
 }
