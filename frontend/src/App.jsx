537b0726dd1cba1b3cc4b37330dabcdf46f0eecc
Varsha Rajawat <89792897+varsha-rajawat@users.noreply.github.com>
2025-05-27 18:38:23 -0400

    Redesign UI (#8)
    * Setting up frontend
    
    * completed login and register functionality with UI
    
    * fixed login issues
    
    * removed unused imports from loginpage
    
    * Fast refresh warning for AuthContext fix
    
    * started frontend work on Job Application CRUD
    
    * Redesign UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,47 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
-import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
-import JobListPage from './pages/jobPages/JobListPage';
-import JobFormPage from './pages/jobPages/JobFormPage';
-// import AddJobPage from './pages/jobPages/AddJobPage';
-// import EditJobPage from './pages/jobPages/EditJobPage' ;
-
+import ViewJobPage from './pages/ViewJobPage';
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="p-4">
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
+
           <Route path="/login" element={
             <PublicRoute>
               <LoginPage />
             </PublicRoute>
           } />
+
           <Route path="/signup" element={
             <PublicRoute>
               <RegisterPage />
             </PublicRoute>
           } />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <DashboardPage />
+
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <DashboardPage />
+            </PrivateRoute>
+          } />
+
+          <Route path="/jobs/:id" element={
+            <PrivateRoute>
+              <ViewJobPage />
               </PrivateRoute>
-            }
-          />
-          <Route path="/jobs" element={<PrivateRoute><JobListPage /></PrivateRoute>} />
-          <Route path="/jobs/new" element={
-            <PrivateRoute>
-              <JobFormPage />
-            </PrivateRoute>
-          } />
-
-          <Route path="/jobs/edit/:id" element={
-            <PrivateRoute>
-              <JobFormPage />
-            </PrivateRoute>
-          } />
+            } />
         </Routes>
-
-        {/* <Route path="/jobs/add" element={<PrivateRoute><AddJobPage /></PrivateRoute>} />
-        <Route path="/jobs/edit/:id" element={<PrivateRoute><EditJobPage /></PrivateRoute>} /> */}
       </main>
     </>
   );
-
 }
 
-export default App
+export default App;
